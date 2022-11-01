71542fc6088fd690bb79c91ae4fb139161ec2631
marisarusso <105472164+marisarusso@users.noreply.github.com>
2022-11-02 09:46:14 +1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+new fullpage ('#fullpage', {
+
+    autoScrolling: true,
+    scrollHorizontally: true,
+    slidesNavigation: true,
+    controlArrows: true,
+    navigation: true,
+    navigationTooltips: ['Home', 'Our Roots', 'Our Plants', 'Easy Care Plants','Our Favourites', 'Gallery', 'Contact Us',],
+    scrollingSpeed: 1200,
+
+})
+
+ 
+    
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+  }
+
+  function togglePopup2(){
+    document.getElementById("popup-2").classList.toggle("active");
+  }
+  
+  function togglePopup3(){
+    document.getElementById("popup-3").classList.toggle("active");
+  }
+
+  function togglePopup4(){
+    document.getElementById("popup-4").classList.toggle("active");
+  }
+  
+  function togglePopup5(){
+    document.getElementById("popup-5").classList.toggle("active");
+  }
+
+  function togglePopup6(){
+    document.getElementById("popup-6").classList.toggle("active");
+  }
+  
+  function togglePopup7(){
+    document.getElementById("popup-7").classList.toggle("active");
+  }
+
+  function togglePopup8(){
+    document.getElementById("popup-8").classList.toggle("active");
+  }
+  
+  function togglePopup9(){
+    document.getElementById("popup-9").classList.toggle("active");
+  }
+
